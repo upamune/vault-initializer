b1be29f45c60e75f6489368e93fd1442814b1164
upamune <info@serizawa.me>
2018-09-05 20:11:10 +0900

    Added implementation for S3

--- storage.go
+++ storage.go
@@ -1,6 +1,14 @@
 package main
 
-import "io"
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
 
 type Storage interface {
 	Get(key string) ([]byte, error)
@@ -8,9 +16,54 @@ type Storage interface {
 }
 
 type S3 struct {
-
+	BucketName string
 }
 
-type GCS struct {
-
+func NewS3(bucketName string) *S3 {
+	return &S3{
+		BucketName: bucketName,
+	}
 }
+
+func (s *S3) getService() (*s3.S3, error) {
+	sess, err := session.NewSession()
+	if err != nil {
+		return nil, err
+	}
+	return s3.New(sess), nil
+}
+
+func (s *S3) Get(key string) ([]byte, error) {
+	svc, err := s.getService()
+	if err != nil {
+		return nil, err
+	}
+
+	obj, err := svc.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(s.BucketName),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		return nil, err
+	}
+	defer obj.Body.Close()
+
+	return ioutil.ReadAll(obj.Body) // FIXME(upamune)
+}
+
+func (s *S3) Put(key string, body []byte) error {
+	svc, err := s.getService()
+	if err != nil {
+		return err
+	}
+
+	_, err = svc.PutObject(&s3.PutObjectInput{
+		Body:   bytes.NewReader(body),
+		Bucket: aws.String(s.BucketName),
+		Key:    aws.String(key),
+	})
+	return err
+}
+
+// TODO
+type GCS struct{}
